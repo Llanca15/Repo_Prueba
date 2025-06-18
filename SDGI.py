d17cbdacef6d6d0374e5a26130eff0afe7327f1c
Llanca15 <lla.flores@duocuc.cl>
2025-06-18 11:15:59 -0400

    Nueva version
    Se actualizo para uso de diccionarios

--- SDGI.py
+++ SDGI.py
@@ -1,7 +1,7 @@
-nombresProductos=[]
-stocksProductos=[]
-preciosProductos=[]
+lista_productos = []
 
+#coigo debe tener 3 validacines:
+#debe tener 5 caracteres, 2 mayusculas y al menos un número
 opcion="0"
 
 """
@@ -12,6 +12,91 @@ Mostrar inventario completo
 Eliminar producto
 Salir
 """
+"Cambia las listar para crear el producto or un diccionario"
+"Agregar una lista para almacenar los dicionarios de productos"
+"Agregar un codigo al diccionario del producto"
+"Modificar las funciones para que utilicen la nueva estructura de dicionario"
+
+def validarcodigo(codigo):
+ 
+    
+    contador_mayusculas=0
+    contador_números= 0
+    for l in codigo:
+        if l.isupper():
+            contador_mayusculas +=1
+        if l.isnumeric():
+            contador_números +=1
+    if contador_mayusculas <2:
+        print("El codigo debe tener al menos 2 mayusculas.")
+        return False
+    elif contador_números==0:
+        print("El codigo debe teer al menos un número.")
+        return False
+    elif len(codigo) <5:
+        print("El codigo debe tener al menos 5 caracteres.")
+        return False
+    else:
+        return True
+
+
+def solicitarProducto():
+        nombre=input("Ingrese el nombre del producto: ")
+        while True:
+            codigo = input("Ingrese el codigo del producto: ")
+            if validarcodigo(codigo)== True:
+                print("Codigo correcto.")
+                break
+            else:
+                print("El codigo es incorreto, vuelva a ingresar.")
+        try:
+            stock=int(input("Ingrese el stock del producto: "))
+            precio=int(input("Ingrese el precio del producto: "))
+            
+            if stock<0 or precio <0:
+                raise ValueError
+                
+            else:
+                producto=[nombre,precio,stock,codigo]
+                return producto
+
+        except ValueError:
+            print("Debe ingresar valores enteros positivos")
+    
+def guardarProducto(nombre,precio,stock,codigo):
+    productobuscado=buscarProducto(codigo)
+    if productobuscado!=None:
+         print("El proucto se encuentra en la lista.")
+         return False
+        
+    producto = {"Nombre:":nombre,"Precio:":precio,"Cantidad:":stock,"Codigo:":codigo}
+    lista_productos.append(producto)
+    return True
+
+
+   
+
+def buscarProducto(codigo):
+    for dictProducto in lista_productos:
+        if codigo==dictProducto["Codigo"]:
+            return dictProducto
+    return None
+
+def mostrarproducto(codigo):
+    productoBuscado=buscarProducto(codigo)
+    if productoBuscado!=None:
+        print("-"*60)
+        print(f"Cod: {productoBuscado["Codigo"]}\tNombre: {productoBuscado["Nombre"]}\tPrecio: {productoBuscado["Precio"]}")
+        print("-"*60)
+    
+
+
+
+
+
+
+
+
 
 while opcion!="6":
     print("1.- Agregar producto")
@@ -23,42 +108,9 @@ while opcion!="6":
 
     opcion=input("Ingrese la opción que desea(1-6): ")
 
-    def solicitarProducto():
-        nombre=input("Ingrese el nombre del producto: ")
-        try:
-            stock=int(input("Ingrese el stock del producto: "))
-            precio=int(input("Ingrese el precio del producto: "))
-            
-            if stock<0 or precio <0:
-                raise ValueError
-                
-            else:
-                producto=[nombre,precio,stock]
-                return producto
-
-        except ValueError:
-            print("Debe ingresar valores enteros positivos")
     
-    def guardarProducto(nombre,precio,stock):
-        if nombre not in nombresProductos:
-            nombresProductos.append(nombre)
-            preciosProductos.append(precio)
-            stocksProductos.append(stock)
-            print("Se guardado correctamente el producto")
-
-    def buscarProducto(nombre):
-        if nombre in nombresProductos:
-            indice= nombresProductos.index(nombre)
-            nombre=nombresProductos[indice]
-            precio=preciosProductos[indice]
-            stock=stocksProductos[indice]
-            print("-"*60)
-            print(f"Nombre: {nombre} \t Precio: ${precio} \t Stock: {stock} unidades")
-            print("-"*60)
-            #return [nombre,precio,stock]
             
-        else:
-            print("No existe un producto con ese nombre")
+   
     
     match opcion:
 
@@ -69,4 +121,6 @@ while opcion!="6":
         case "2":
             nombreProducto=input("Ingrese el nombre del producto a buscar: ")
             buscarProducto(nombreProducto)
+        case "3":
+
 
